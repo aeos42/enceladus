a67a8be2326f34992070c06332a358475a2de9f2
Adam Siefkas <adam.siefkas@sphero.com>
2017-11-25 16:27:49 -0700

    basic mouse lookaround and WASD movement

---
+++ Assets/Scripts/firstPersonPlayerBehavior.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class firstPersonPlayerBehavior : MonoBehaviour
+{
+	public float movementSpeed = 5.0f;
+	
+	// Use this for initialization
+	void Start () 
+	{
+			
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+		//mouse
+		float rotLeftRight = Input.GetAxis("Mouse X");
+		float rotUpDown = Input.GetAxis("Mouse Y");
+		
+		transform.Rotate(-rotUpDown, rotLeftRight, 0);
+		
+		//WASD movement
+		float forwardSpeed = Input.GetAxis("Vertical")* movementSpeed;
+		float sideSpeed = Input.GetAxis("Horizontal") * movementSpeed;
+		
+		
+		Vector3 speed = new Vector3(sideSpeed, 0, forwardSpeed);
+
+		CharacterController cc = GetComponent<CharacterController>();
+
+		cc.SimpleMove(speed);
+		
+		
+	}
+}
